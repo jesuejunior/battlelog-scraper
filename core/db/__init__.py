682d13300ffb3a4e57368ff1d2d867f26ec1a85a
Jesue Junior <jesuesousa@gmail.com>
2013-08-02 00:24:27 -0300

    Acertando profile e deixando usavel, com arquitetura melhorada

--- core/db/__init__.py
+++ core/db/__init__.py
@@ -1,11 +1,20 @@
 __author__ = 'jesuejunior'
+import os
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.schema import MetaData
 from sqlalchemy import create_engine
 
+is_test = os.environ.get("TEST", "1") == "1"
 
-engine = create_engine("postgresql+psycopg2://postgres:vivo22@localhost:5432/battlelog")
+CONN_STRING = "postgresql+psycopg2://postgres:vivo22@localhost:5432/battlelog"
+
+if is_test:
+    CONN_STRING = "sqlite+pysqlite:///:memory:"
+
+CONN_STRING = os.environ.get("CONN_STRING", "sqlite+pysqlite:///:memory:")
+
+engine = create_engine(CONN_STRING)
 Metadata = MetaData(bind=engine)
 Model = declarative_base(metadata=Metadata)
 Session = sessionmaker(bind=engine)
